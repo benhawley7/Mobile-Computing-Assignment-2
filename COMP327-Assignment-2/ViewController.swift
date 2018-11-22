417f064e6406d9a8b2e03ee57cf8003b5848a509
Ben Hawley <ben.hawley@digitaldesignlabs.com>
2018-11-22 21:22:30 +0000

    Initial Commit

---
+++ COMP327-Assignment-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COMP327-Assignment-2
+//
+//  Created by Ben Hawley on 22/11/2018.
+//  Copyright © 2018 Ben Hawley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
